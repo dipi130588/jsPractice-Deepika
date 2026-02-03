076d7300bc2c02be3a3aa8df1082e7c2c389d953
dipi130588 <deepika.maurya.c@gmail.com>
2026-02-03 22:13:31 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const accountId = 14453
+let accountEmail= "abc01@gmail.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treats all Javascript code as newer version
+let name="deepika"
+let age="30"
+let isLoggedIn=false
+
+//Range of number datatype is 2 to power 53
+//bingint datatyoe is used for big numbers
+//string datatype->'' and "" both can be use
+//boolean datatype->true or false value
+//null datatype has standalone value
+//undefined datatype has undefined value
+//symbol datatype used for uniqueness
+
+let accountType
+
+console.log(typeof "deepika") // typeof is used to determine type of an element
+console.log(typeof null) //output is object
+console.log(typeof undefined) // output is undefined
