3a75c0cdee92c5d8a1a899d21887317ec5012f23
dipi130588 <deepika.maurya.c@gmail.com>
2026-02-04 05:02:35 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+/*Conversion to Number
+let score= 33
+let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+Notes:
+"33" gets converted to 33
+"33abc" gets converted to NaN
+true gets converted to 1
+false gets converted to 0 */
+
+/*----------------------------------*/
+/*Conversion to boolean
+let isLoggedIn="abc"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+Notes:
+1 gets converted to true
+0 gets converted to false
+"" gets converted to false
+"hitesh" gets converted to true */
+
+/*-----------------------------------*/
+/*Conversion to String*/
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
